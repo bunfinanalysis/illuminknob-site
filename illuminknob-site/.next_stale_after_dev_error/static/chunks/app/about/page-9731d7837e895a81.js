88ef93528e247de8ed54522362ca2c77406f806e
bunfinanalysis <bunnyfinancialanalysis@gmail.com>
2026-04-09 12:48:45 -0400

    Fix Vercel Next.js detection

--- illuminknob-site/.next_stale_after_dev_error/static/chunks/app/about/page-9731d7837e895a81.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{9541:(e,s,n)=>{Promise.resolve().then(n.bind(n,5477)),Promise.resolve().then(n.t.bind(n,2619,23))}},e=>{e.O(0,[791,441,255,358],()=>e(e.s=9541)),_N_E=e.O()}]);
